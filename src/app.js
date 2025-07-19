79ce5f4e97bd26ebf2df20752e8f280768546c72
Lakshya Shahi <lakshyashahi0712@gmail.com>
2025-07-19 22:50:53 +0530

    make get , delete ,post , patch and head call

--- src/app.js
+++ src/app.js
@@ -1,13 +1,31 @@
 const express = require("express");
 
-const app = express()
+const app = express();
 
-app.use("/test",(req,res)=>{
-    res.send("hello from test")
+app.get("/user",(req,res)=>{
+    res.send("get call from user");
 })
-//
-app.use((req,res)=>{
-    res.send("hello from server")
+
+app.post("/user",(req,res)=>{
+    res.send("post call from user");
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("delete call from user");
+})
+
+app.patch("/user",(req,res)=>{
+    res.send("patch call from user");
+})
+
+app.head("/user",(req,res)=>{
+    res.send("head call from user");
+})
+
+
+
+app.use("/test/hello",(req,res)=>{
+    res.send("hello from hello")
 })
 
 app.listen(3000,()=>{
