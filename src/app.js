31188de34b2d3d66b23db9d01861f876029abc22
Lakshya Shahi <lakshyashahi0712@gmail.com>
2025-07-19 21:52:18 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express()
+
+app.use("/test",(req,res)=>{
+    res.send("hello from test")
+})
+//
+app.use((req,res)=>{
+    res.send("hello from server")
+})
+
+app.listen(3000,()=>{
+    console.log("server started")
+})
