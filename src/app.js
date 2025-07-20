31211cee456ab6b7e98eb5c23319e828749777fe
Lakshya Shahi <lakshyashahi0712@gmail.com>
2025-07-20 14:41:35 +0530

    muliple route functions

--- src/app.js
+++ src/app.js
@@ -3,9 +3,16 @@ const express = require("express");
 const app = express();
 
 // using this url http://localhost:3000/user?userId=100&name=lak will give in console userId=100  name=lak
-app.get("/user",(req,res)=>{
-    console.log(req.query)
-    res.send("get call from user");
+app.get("/user",(req,res,next)=>{
+    console.log("1st route")
+    //res.send("get call from user");
+
+    next();
+},(req,res,next)=>{
+
+console.log("2nd route")
+res.send("2nd response")
+
 })
 // using this url http://localhost:3000/user/100/lak will give in console userid:100 and name:lak 
 // app.get("/user/:userId/:name",(req,res)=>{
