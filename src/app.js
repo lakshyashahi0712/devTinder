ca0d44bd8fabefd61ab55521ec1ba6c692c3c1fb
Lakshya Shahi <lakshyashahi0712@gmail.com>
2025-07-29 23:25:25 +0530

    encrypting password

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const { connectDB } = require("./config/database");
 const { User } = require("./models/user");
+const {validateSignUpData} = require("./utils/validate")
+const bcrypt = require("bcrypt")
+
 
 const app = express();
 console.log("✅ App instance created");
@@ -9,15 +12,46 @@ app.use(express.json());
 console.log("✅ Middleware added");
 
 
-
 app.post("/signup" , async(req,res)=>{
-  const user = new User(req.body);
-  try{
+try{
+  //validation of data
+  validateSignUpData(req);
+
+  const {firstName , lastName , email , password} = req.body;
+
+  //encrypt password
+  const paswordHash = await bcrypt.hash(password,10);
+  console.log(paswordHash);
+
+  //creating new instance for user model
+  const user = new User({firstName , lastName , email , password:paswordHash})
+
     await user.save();
     res.send("user added successfully");
   }catch(err){
     res.status(400).send("error saving the user" + err.message);
   }
+})
+
+
+
+app.post("/login",async(req,res)=>{
+  try{
+  const {email,password} = req.body;
+
+  const user = await User.findOne({email:email});
+  if(!user){
+    throw new Error("invalid credintials");
+  }
+  const isPasswordValid = await bcrypt.compare(password,user.password);
+  if(isPasswordValid){
+    res.send("login success")
+  } else{
+    throw new Error("invalid credintials")
+  }
+}catch(err){
+  res.status(400).send("error saving the user" + err.message);
+}
 })
 
 app.get("/user",async(req,res)=>{
---
+++ src/utils/validate.js
@@ -0,0 +1,16 @@
+const validator = require("validator")
+
+const validateSignUpData =(req)=>{
+
+    const {firstName , lastName , email , password} = req.body;
+
+    if(!firstName || !lastName){
+        throw new Error("Enter valid name");
+    } else if(!validator.isEmail(email)){
+        throw new Error("Enter valid email")
+    }
+}
+
+module.exports = {
+    validateSignUpData
+}
