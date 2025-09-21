a93b165c3d1aaf70dd7a3134365becf5509cac71
Lakshya Shahi <lakshyashahi0712@gmail.com>
2025-09-21 22:31:30 +0530

    added fe url

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ const app = express();
 console.log("✅ App instance created");
 
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true, }) )
+app.use(cors({ origin: "https://devtinder-web-s67v.onrender.com", credentials: true, }) )
 
 
 app.use(express.json());
